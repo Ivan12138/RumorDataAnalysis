b3634331da9067271859196536e0b32db62fade7
snow <xueyao_98@foxmail.com>
2018-11-27 11:15:29 +0800

    Text Filtering: 4-fold Filtering Done

--- sampling/tf_idf_text_filtering_truth.py
+++ sampling/tf_idf_text_filtering_truth.py
@@ -9,47 +9,6 @@ import random
 import TfIdf_SinglePass
 
 
-def show_clustering_truth():
-    single_pass_cluster = joblib.load('file/pkl/tf_idf_{}_clustering.pkl'.format('truth_1'))
-    cluster_list = single_pass_cluster.cluster_list
-
-    # 输出过滤后的文本内容
-    with open('file/corpus/corpus_of_truth.txt', 'r', encoding='utf-8') as src:
-        with open('file/clustering/truth_1.txt', 'w', encoding='utf-8') as out:
-            lines = src.readlines()
-            for cluster in cluster_list:
-                for i in cluster.node_list:
-                    out.write('{}'.format(lines[i]))
-                out.write('-----------------------------------\n')
-                out.flush()
-
-    # 统计微博数量、图片数量、userCertify分布
-    with open('file/weibo_truth_text_filtered.json', 'r', encoding='utf-8') as src:
-        lines = src.readlines()
-        filtered_weibo_num = len(lines)
-        filtered_pic_num = 0
-        certify_0 = 0
-        certify_1 = 0
-        certify_2 = 0
-
-        for line in lines:
-            truth = json.loads(line, encoding='utf-8-sig')
-            filtered_pic_num += len(truth['piclist'])
-            if 'userCertify' in truth.keys():
-                certify = truth['userCertify']
-                if certify == 0:
-                    certify_0 += 1
-                elif certify == 1:
-                    certify_1 += 1
-                else:
-                    certify_2 += 1
-
-        print('聚类后的真实微博：数量为{}，图片数量为{}'.format(filtered_weibo_num, filtered_pic_num))
-        print('（{}）{}:{}:{} = {:.1f} : 1 : {:.1f}'.format(
-            certify_0 + certify_1 + certify_2, certify_0, certify_1,
-            certify_2, certify_0 / certify_1, certify_2 / certify_1))
-
-
 # 把15.8w的数据分成4份，分别聚类
 def clustering_truth_to_4_fold(threshold=0.6):
     _, tf_idf = joblib.load('file/pkl/tf_idf_of_{}.pkl'.format('truth_4000'))
@@ -85,6 +44,7 @@ def gen_clustering(num):
                 out.flush()
 
 
+# 生成4折过滤后的语料，供再次过滤
 def gen_filtered_truth_from_4_fold():
     with open('file/corpus/corpus_of_truth.txt', 'r', encoding='utf-8') as src:
         lines = src.readlines()
@@ -174,4 +134,138 @@ def clustering_truth_from_4_fold(threshold=0.6):
     joblib.dump(single_pass_cluster, 'file/pkl/tf_idf_{}_clustering.pkl'.format('truth_4_fold'))
 
 
-clustering_truth_from_4_fold()
+# 展示2次聚类的结果
+def show_twice_clustering_truth():
+    single_pass_cluster = joblib.load('file/pkl/tf_idf_{}_clustering.pkl'.format('truth_4_fold'))
+    cluster_list = single_pass_cluster.cluster_list
+
+    # 输出过滤后的文本内容
+    with open('file/corpus/corpus_of_truth_4_fold.txt', 'r', encoding='utf-8') as src:
+        with open('file/clustering/truth_4_fold.txt', 'w', encoding='utf-8') as out:
+            lines = src.readlines()
+            for cluster in cluster_list:
+                for i in cluster.node_list:
+                    out.write('{}'.format(lines[i]))
+                out.write('-----------------------------------\n')
+                out.flush()
+
+    # 得到最终的json文件
+    with open('../weibo_truth_analysis/file/weibo_truth.txt', 'r', encoding='utf-8') as src:
+        events = src.readlines()
+
+    out = open('file/weibo_truth_4_fold_text_filtered.json', 'w')
+    out_pretty = open('file/weibo_truth_4_fold_text_filtered_pretty.json', 'w')
+
+    valid_cluster_num = 0
+    missing_pics_cluster_num = 0
+
+    for cluster in cluster_list:
+        truth_weibos = []
+
+        for index in cluster.node_list:
+            line = lines[index]
+            event_index = int(line.split(',')[0][1:])
+            weibo_index = int(line.split(',')[1].split(')')[0][1:])
+
+            event = json.loads(events[event_index], encoding='utf-8')
+            truth_weibo = event['weibo'][weibo_index]
+
+            if 'piclist' in truth_weibo.keys() and isinstance(truth_weibo['piclist'], list):
+                if len(truth_weibo['piclist']) != 0:
+                    truth_weibo['_position'] = (event_index, weibo_index)
+                    truth_weibos.append(truth_weibo)
+
+        # 选取规则：在有图片的微博中随机取
+        if len(truth_weibos) <= 0:
+            missing_pics_cluster_num += 1
+            continue
+        valid_cluster_num += 1
+        chosen_truth = random.sample(truth_weibos, 1)[0]
+
+        out.write('{}\n'.format(json.dumps(chosen_truth, ensure_ascii=False)))
+        out.flush()
+        out_pretty.write('{}\n'.format(json.dumps(chosen_truth, ensure_ascii=False, indent=4, separators=(',', ':'))))
+        out_pretty.flush()
+
+    print('有效的簇为{}个，缺少图片的簇为{}个'.format(valid_cluster_num, missing_pics_cluster_num))
+
+    # 统计微博数量、图片数量、userCertify分布
+    with open('file/weibo_truth_4_fold_text_filtered.json', 'r', encoding='utf-8') as src:
+        lines = src.readlines()
+        filtered_weibo_num = len(lines)
+        filtered_pic_num = 0
+        certify_0 = 0
+        certify_1 = 0
+        certify_2 = 0
+
+        for line in lines:
+            truth = json.loads(line, encoding='utf-8-sig')
+            filtered_pic_num += len(truth['piclist'])
+            if 'userCertify' in truth.keys():
+                certify = truth['userCertify']
+                if certify == 0:
+                    certify_0 += 1
+                elif certify == 1:
+                    certify_1 += 1
+                else:
+                    certify_2 += 1
+
+        print('聚类后的真实微博：数量为{}，图片数量为{}'.format(filtered_weibo_num, filtered_pic_num))
+        print('（{}）{}:{}:{} = {:.1f} : 1 : {:.1f}'.format(
+            certify_0 + certify_1 + certify_2, certify_0, certify_1,
+            certify_2, certify_0 / certify_1, certify_2 / certify_1))
+
+
+# 展示1次聚类的结果
+def show_once_clustering_truth():
+    # 得到最终的json文件
+    with open('../weibo_truth_analysis/file/weibo_truth.txt', 'r', encoding='utf-8') as src:
+        events = src.readlines()
+
+    out = open('file/weibo_truth_once_text_filtered.json', 'w')
+    out_pretty = open('file/weibo_truth_once_text_filtered_pretty.json', 'w')
+    with open('file/corpus/corpus_of_truth_4_fold.txt', 'r') as src:
+        lines = src.readlines()
+        for line in lines:
+            event_index = int(line.split(',')[0][1:])
+            weibo_index = int(line.split(',')[1].split(')')[0][1:])
+
+            event = json.loads(events[event_index], encoding='utf-8')
+            truth_weibo = event['weibo'][weibo_index]
+            truth_weibo['_position'] = (event_index, weibo_index)
+
+            out.write('{}\n'.format(json.dumps(truth_weibo, ensure_ascii=False)))
+            out.flush()
+            out_pretty.write('{}\n'.format(json.dumps(truth_weibo, ensure_ascii=False, indent=4, separators=(',', ':'))))
+            out_pretty.flush()
+    out.close()
+    out_pretty.close()
+
+    # 统计微博数量、图片数量、userCertify分布
+    with open('file/weibo_truth_once_text_filtered.json', 'r', encoding='utf-8') as src:
+        lines = src.readlines()
+        filtered_weibo_num = len(lines)
+        filtered_pic_num = 0
+        certify_0 = 0
+        certify_1 = 0
+        certify_2 = 0
+
+        for line in lines:
+            truth = json.loads(line, encoding='utf-8-sig')
+            filtered_pic_num += len(truth['piclist'])
+            if 'userCertify' in truth.keys():
+                certify = truth['userCertify']
+                if certify == 0:
+                    certify_0 += 1
+                elif certify == 1:
+                    certify_1 += 1
+                else:
+                    certify_2 += 1
+
+        print('聚类后的真实微博：数量为{}，图片数量为{}'.format(filtered_weibo_num, filtered_pic_num))
+        print('（{}）{}:{}:{} = {:.1f} : 1 : {:.1f}'.format(
+            certify_0 + certify_1 + certify_2, certify_0, certify_1,
+            certify_2, certify_0 / certify_1, certify_2 / certify_1))
+
+
+show_once_clustering_truth()
